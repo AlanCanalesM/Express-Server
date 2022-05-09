8d17879c071fcd0bd9af8e6ade39fe0fddde85d5
AlanCanalesM <alan.canales@micorreo.upp.edu.mx>
2022-05-08 19:11:51 -0500

    The server was created succesfully

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ const express = require('express')
 const app = express()
 //I select the port where i look our app: localhost: 3000
 const port = 3000
+
+//initial path it answer to url localhost: 3000
+app.get('/', (req, res)=>{
+
+    res.send('Hello world!')
+
+})
+
+//with this initializing this app
+app.listen(port, () =>{
+
+    console.log('Example app listening on port ${port}')
+})
